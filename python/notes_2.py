39e069464e57928cedaffe73a41f1d2005944109
nbsjay <nathanbrownsmith@outlook.com>
2022-07-14 03:59:07 +0000

    update note.py files

---
+++ python/notes_2.py
@@ -0,0 +1,15 @@
+"""
+when wtiting code, pay attention to the order of operations. Use parentheses to help prevent bugs and
+make the intentions of your code clearer.
+"""
+
+"""
+like int() and float() which convert to their respective data types, bool() converts values to boolean
+numbers are converted to 'True', '0' is converted to false, strings are converted to 'True' and and empty
+string is converted to 'False'
+"""
+
+print(bool(1))
+print(bool(0))
+print(bool("abc"))
+print(bool("")) # take care to not input a space in the quotes
