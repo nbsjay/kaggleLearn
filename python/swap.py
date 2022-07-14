5b7e8ebd210434d18dfea32d265071dfbe0eb92a
nbsjay <nathanbrownsmith@outlook.com>
2022-07-14 19:28:55 +0000

    added swap.py

---
+++ python/swap.py
@@ -0,0 +1,21 @@
+# swap first and last positions in a list
+
+def swap(r):
+    """
+    Given a list of racers, replace the first placed racer with the last
+    and vice versa.
+
+    >>> list_of _racers = ["a", "b", "c", "d]
+    >>> x = swap(list_of_racers)
+    >>> print(x)
+    >>> ["d", "b", "c", "a"]
+    """
+    
+    r_temp = r[0]
+    r[0] = r[-1]
+    r[-1] = r_temp
+    return r
+
+racers = ["Carlton", "Joe", "Theophilus", "Michael", "Oscar", "Kofi", "Junior" ]
+
+print(swap(racers))
