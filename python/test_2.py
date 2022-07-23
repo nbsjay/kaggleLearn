d8cb159cac93cd72d55a056be2008290270a2f63
nbsjay <nathanbrownsmith@outlook.com>
2022-07-23 19:18:12 +0000

    update

--- python/test_2.py
+++ python/test_2.py
@@ -1,5 +1,6 @@
-txt1 = "My name is {fname}, I'm {age}".format(fname = "John", age = 36)
-txt2 = "My name is {0}, I'm {1}".format("John",36)
-txt3 = "My name is {}, I'm {}".format("John",36)
+numbers = {'one':1, 'two':2, 'three':3}
+numbers["four"] = "4"
 
-print(txt1, "\n", txt2, "\n", txt3)
+for k in numbers:
+    print("{} = {}".format(k, numbers[k]))
+
